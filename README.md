c6a63c735bef09d77d47e4212e2863e7a9050fc4
Owen Tourlamain <owen.tourlamain@gmail.com>
2017-05-08 05:29:25 +0100

    tidying file structure ready for submission

--- README.md
+++ README.md
@@ -1,7 +1,47 @@
-# ACE
 ACE: Data capture application for the Aberystwyth University PanCam Emulator (AUPE)
+- Author:     Owen Tourlamain
+- Supervisor: Dr. Laurence Tyler
+
+Usage:
+ ace-ng [-fv]
+
+ -f, --faupe,   Skip connection to AUPE server and launch FAUPE server
+ -v, --verbose, Print more information to the console
 
 Dependencies:
  - Python 3.4
+ - Python 2.X
  - PyQt5
  - Pillow
+
+Notable system features:
+- Dynamic language files:
+  Language files should be stored in ACE-NG/lang/ or in the user configuable
+  location (defaults to ~/.ace-ng/lang/).
+
+  Files should be JSON formatted, look in the en-GB file for the list of
+  properties required.
+
+  Files should be named using a combination of ISO 639-1 and 3166-1. For
+  example the file for UK English is: "en-GB.json" and Mexican Spanish is:
+  "es-MX.json".
+
+  Users can specify which language should be used through the "lang_pref"
+  config setting, the value should match the file name minus the suffix. For
+  example: "en-GB" or "es-MX".
+
+  If a file is missing a property then this library will fall back to the next
+  relevant choice. The library prioritises the file from the users lang
+  directory first, then the matching lang file in ACE-NG/lang/ (if any), then
+  finally the default en-GB file as this is assumed to always be complete.
+
+- Extras:
+  Extension module template included, as well as a simple example extension,
+  calibrate.py. Move this file to ext to load it into ACE-NG.
+
+Acknowledgements:
+- lib/au:
+  Provided by Dr. Laurence Tyler, used without modification.
+
+- lang/cy-GB.json:
+  File created by the author, translation services provided by James Cox (jsc12@aber.ac.uk)
--- lib/dynamicProperties.py
+++ lib/dynamicProperties.py
@@ -16,30 +16,6 @@ class DynamicProperties(object):
         from lib.dynamicProperties import DynamicProperties
         strings = DynamicProperties("lang/en-GB.json")
         print(strings.appName)
-
-    # move to different file
-    Language files should be stored in ACE-NG/lang/ or in the user configuable
-    location (defaults to ~/.ace-ng/lang/).
-
-    Files should be JSON formatted, look in the en-GB file for the list of
-    properties required.
-
-    Files should be named using a combination of ISO 639-1 and 3166-1. For
-    example the file for UK English is: "en-GB.json" and Mexican Spanish is:
-    "es-MX.json".
-
-    Users can specify which language should be used through the "lang_pref"
-    config setting, the value should match the file name minus the suffix. For
-    example: "en-GB" or "es-MX".
-
-    If a file is missing a property then this library will fall back to the next
-    relevant choice. The library prioritises the file from the users lang
-    directory first, then the matching lang file in ACE-NG/lang/ (if any), then
-    finally the default en-GB file as this is assumed to always be complete.
-
-    The library can be used by initialising it with a populated config
-    dictionary, then accessing the properties as python module attributes. E.g:
-
     """
 
     def __init__(self, defaultFile, verbose, cleanPaths=False):
@@ -105,8 +81,6 @@ class DynamicProperties(object):
                 self.dict[item] = os.path.expanduser(self.dict[item])
 
 class MalformedDataError(Exception):
-    # TODO: docstrings
-    """docstring for MalformedDataError."""
     def __init__(self, value):
         self.value = value
 
@@ -114,8 +88,6 @@ class MalformedDataError(Exception):
         return repr(self.value)
 
 class FileAccessError(Exception):
-    # TODO: docstrings
-    """docstring for FileAccessError."""
     def __init__(self, value):
         self.value = value
 
